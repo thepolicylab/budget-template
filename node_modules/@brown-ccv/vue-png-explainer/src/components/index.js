5a441d27257e9b475034bd4648d43d4c452695f3
Ben Guhin Delphine <benguhin@users.noreply.github.com>
2020-07-21 14:17:48 -0500

    push to pages

---
+++ node_modules/@brown-ccv/vue-png-explainer/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Explainer } from './Explainer.vue';
+export { default as ExplainerImage } from './ExplainerImage.vue';
+export { default as ExplainerImageHighlight } from './ExplainerImageHighlight.vue';
+export { default as Expandable } from './Expandable.vue';
